cf36989e091a975b103e0bd281552409f0820a0a
klauddius <klauddius@gmail.com>
2021-05-26 01:27:13 -0300

    Creates class to solve SubArray With Target Sum challenge

---
+++ src/main/java/br/com/klauddius/dailyproblems/SubArrayWithTargetSum.java
@@ -0,0 +1,26 @@
+package br.com.klauddius.dailyproblems;
+
+public class SubArrayWithTargetSum {
+
+    /**
+     * Hi, here's your problem today. This problem was recently asked by Amazon:
+     * <p>
+     * You are given an array of integers, and an integer K. Return the subarray which sums to K. You can assume that a solution will always exist.
+     * <p>
+     * def find_continuous_k(list, k):
+     * # Fill this in.
+     * <p>
+     * print find_continuous_k([1, 3, 2, 5, 7, 2], 14)
+     * # [2, 5, 7]
+     * <p>
+     * Can you do this in linear time?
+     *
+     * @param array containing input data
+     * @param k representing the target sum
+     * @return subarray with all items which sums to k
+     */
+    public int[] findContinuousK(int[] array, int k) {
+        // TODO Fill me
+        return new int[]{0};
+    }
+}
