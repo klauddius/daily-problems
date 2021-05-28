11427c5024efd3ac035aef423e9c29540e6c4c06
klauddius <klauddius@gmail.com>
2021-05-27 23:44:48 -0300

    Solve the subarray with target sum challenge.

--- src/main/java/br/com/klauddius/dailyproblems/SubArrayWithTargetSum.java
+++ src/main/java/br/com/klauddius/dailyproblems/SubArrayWithTargetSum.java
@@ -1,11 +1,14 @@
 package br.com.klauddius.dailyproblems;
 
+import java.util.ArrayList;
+
 public class SubArrayWithTargetSum {
 
     /**
      * Hi, here's your problem today. This problem was recently asked by Amazon:
      * <p>
-     * You are given an array of integers, and an integer K. Return the subarray which sums to K. You can assume that a solution will always exist.
+     * You are given an array of integers, and an integer K. Return the subarray which sums to K.
+     * You can assume that a solution will always exist.
      * <p>
      * def find_continuous_k(list, k):
      * # Fill this in.
@@ -16,11 +19,40 @@ public class SubArrayWithTargetSum {
      * Can you do this in linear time?
      *
      * @param array containing input data
-     * @param k representing the target sum
+     * @param k     representing the target sum
      * @return subarray with all items which sums to k
      */
-    public int[] findContinuousK(int[] array, int k) {
-        // TODO Fill me
-        return new int[]{0};
+    public ArrayList<Integer> findContinuousK(int[] array, int k) throws Exception {
+        ArrayList<Integer> result = new ArrayList<>();
+        int sum;
+        boolean subarrayFound = false;
+        for (int i = 0; i < array.length; i++) {
+            result.clear();
+            sum = array[i];
+            result.add(array[i]);
+            for (int j = i + 1; j < array.length; j++) {
+                sum += array[j];
+                result.add(array[j]);
+                if (sum >= k) {
+                    break;
+                }
+            }
+            if (sum == k) {
+                subarrayFound = true;
+                break;
+            }
+        }
+        if (!subarrayFound) {
+            throw new Exception("Subarray Not Found");
+        }
+        return result;
     }
+
+    public static void main(String[] args) throws Exception {
+        SubArrayWithTargetSum s = new SubArrayWithTargetSum();
+
+        int[] testArray = {1, 3, 2, 5, 7, 2};
+        s.findContinuousK(testArray, 14);
+    }
+
 }
--- src/test/java/br/com/klauddius/test/dailyproblems/SubArrayWithTargetSumTest.java
+++ src/test/java/br/com/klauddius/test/dailyproblems/SubArrayWithTargetSumTest.java
@@ -4,41 +4,67 @@ import br.com.klauddius.dailyproblems.SubArrayWithTargetSum;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class SubArrayWithTargetSumTest {
 
     SubArrayWithTargetSum sawts = new SubArrayWithTargetSum();
 
     @Test
-    public void testKFoundInOrderedArray() {
-        int[] orderedArray = new int[]{1, 2, 3, 4, 5};
-        int k = 6;
+    public void testFoundKInTheMiddleOfArray() throws Exception {
+        int[] orderedArray = new int[]{1, 3, 2, 5, 7, 2};
+        int k = 14;
 
-        int[] expecteds = {1, 2, 3};
-        int[] actuals = sawts.findContinuousK(orderedArray, k);
-
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testKFoundInAnUnorderedArray() {
-        int[] unorderedArray = new int[]{1, 4, 5, 2, 3};
-        int k = 6;
-
-        int[] expecteds = {4, 2};
-        int[] actuals = sawts.findContinuousK(unorderedArray, k);
+        Integer[] expecteds = {2, 5, 7};
+        List<Integer> result = sawts.findContinuousK(orderedArray, k);
+        Integer[] actuals = result.toArray(new Integer[0]);
 
         Assert.assertArrayEquals(expecteds, actuals);
     }
 
     @Test
-    public void testKNotFoundInArray() {
+    public void testFoundKInTheStartOfAnArray() throws Exception {
+        int[] unorderedArray = new int[]{1, 3, 2, 5, 7, 2};
+        int k = 6;
+
+        Integer[] expecteds = {1, 3, 2};
+        List<Integer> result = sawts.findContinuousK(unorderedArray, k);
+        Integer[] actuals = result.toArray(new Integer[0]);
+
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testFoundKInTheEndOfAnArray() throws Exception {
+        int[] unorderedArray = new int[]{1, 3, 2, 5, 7, 9, 22};
+        int k = 38;
+
+        Integer[] expecteds = {7, 9, 22};
+        List<Integer> result = sawts.findContinuousK(unorderedArray, k);
+        Integer[] actuals = result.toArray(new Integer[0]);
+
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testFoundKInAnArrayWithSubarrayOfSizeOne() throws Exception {
+        int[] unorderedArray = new int[]{1, 3, 2, 5, 7, 2};
+        int k = 2;
+
+        Integer[] expecteds = {2};
+        List<Integer> result = sawts.findContinuousK(unorderedArray, k);
+        Integer[] actuals = result.toArray(new Integer[0]);
+
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test(expected = Exception.class)
+    public void testKNotFoundInArray() throws Exception {
         int[] array = new int[]{1, 2, 3};
         int k = 12;
 
-        int[] expecteds = new int[0];
-        int[] actuals = sawts.findContinuousK(array, k);
-
-        Assert.assertArrayEquals(expecteds, actuals);
+        Integer[] expecteds = {};
+        List<Integer> result = sawts.findContinuousK(array, k);
     }
 
 }
